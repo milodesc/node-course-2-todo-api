e2b2d614a138efb16698e939d0408fa02fc8bfb6
Pat Jensen <pjensen@aclu.org>
2018-07-19 21:07:11 -0400

    Test the DELETE /todos/:id route.

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -103,3 +103,42 @@ describe('GET /todos:id', () => {
     });
 
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((e) => done(e));
+            })
+    });
+
+    it('should return a 404 if todo not found', (done) => {
+        testId = new ObjectId().toHexString();
+        request(app)
+            .delete(`/todos/${testId}`)
+            .expect(404)
+            .end(done);;
+    });
+
+    it('should return a 404 for non-ObjectIds', (done) => {
+        request(app)
+            .delete(`/todos/123`)
+            .expect(404)
+            .end(done);
+    });
+
+});

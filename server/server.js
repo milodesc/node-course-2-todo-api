048562564950acf92137c098664c326a5d2f40b6
Pat Jensen <pjensen@aclu.org>
2018-07-30 20:47:35 -0400

    Add POST /users/login route.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectId} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -106,6 +107,18 @@ app.post('/users', (req, res) => {
         res.status(400).send(e);
     });
 });
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((err) => {
+        res.status(400).send();
+    })
+})
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
